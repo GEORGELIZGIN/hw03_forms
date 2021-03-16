89c6370b62a0a13c3733b690039d9c5b2accd112
Georgiy <lizgin.gv@phystech.edu>
2021-03-17 01:11:35 +0300

    commit

--- yatube/settings.py
+++ yatube/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 LOGIN_URL = "/auth/login/"
-LOGIN_REDIRECT_URL = "index" 
+LOGIN_REDIRECT_URL = "index"
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails") 
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
