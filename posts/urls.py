3ddb5039a2e28cba3a26a2ae589d20519536fcfe
Georgiy <lizgin.gv@phystech.edu>
2021-03-17 01:03:30 +0300

    commit

---
+++ posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path('group/<str:slug>/', views.group_posts),
+    path('new/', views.NewPostView.as_view(), name='new_post'),
+]
---
+++ posts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Post, Group, User
+from .forms import PostForm
+
+
+def index(request):
+    latest = Post.objects.all()[:11]
+    return render(request, "index.html", {"posts": latest})
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
+
+
+class NewPostView(LoginRequiredMixin, CreateView):
+    form_class = PostForm
+    success_url = "/"
+    template_name = "new_post.html"
+
+    def form_valid(self, form):
+        new_post = form.save(commit=False)
+        new_post.author = self.request.user
+        new_post.save()
+        return super().form_valid(form)
